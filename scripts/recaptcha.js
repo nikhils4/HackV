761736af6a8c153858e6f295a290a9a908936e62
Nikhil Singh <nikhilsingh498@gmail.com>
2019-06-14 02:53:45 +0530

    Remove recaptcha external file and links

--- scripts/recaptcha.js
+++
@@ -1,5 +0,0 @@
-grecaptcha.ready(function() {
-    grecaptcha.execute('6LdwaqgUAAAAAHq8aXnOCQBhTaMh9vFsDlZ_ikZ_', {action: 'homepage'}).then(function(token) {
-        console.log(token);
-    });
-});
