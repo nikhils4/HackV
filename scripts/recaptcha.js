9195b1c839a63d77b6639c40634a73a5feb41455
Nikhil Singh <nikhilsingh498@gmail.com>
2019-06-13 06:11:42 +0530

    Recaptcha init for all the forms

---
+++ scripts/recaptcha.js
@@ -0,0 +1,5 @@
+grecaptcha.ready(function() {
+    grecaptcha.execute('6LdwaqgUAAAAALRyAOfD35aCJgt3s1W-S3r5ir58', {action: 'homepage'}).then(function(token) {
+        // response stuff here 
+    });
+});
