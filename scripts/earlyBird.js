0575be34fc221a5cff142ca59a271a203804c239
Nikhil Singh <nikhilsingh498@gmail.com>
2019-06-19 04:28:42 +0530

    Update blog link

--- scripts/earlyBird.js
+++ scripts/earlyBird.js
@@ -1,10 +1,10 @@
-document.getElementById("blog").addEventListener("click", (e) => {
-    document.getElementById("popup").style.display = "block";
-    document.getElementById("popup").classList.add("animated", "fadeIn", "fast");
-    document.getElementById("popup-img").src = "../asset/images/comingsoon.gif";
-    document.getElementById("popup-head").innerHTML = "Coming Soon";
-    document.getElementById("popup-description").innerHTML = "We will be soon here";
-})
+// document.getElementById("blog").addEventListener("click", (e) => {
+//     document.getElementById("popup").style.display = "block";
+//     document.getElementById("popup").classList.add("animated", "fadeIn", "fast");
+//     document.getElementById("popup-img").src = "../asset/images/comingsoon.gif";
+//     document.getElementById("popup-head").innerHTML = "Coming Soon";
+//     document.getElementById("popup-description").innerHTML = "We will be soon here";
+// })
 
 document.getElementById("close-btn").addEventListener("click", (e) => {
     document.getElementById("popup").style.display = "none";
