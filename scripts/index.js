74dfe1cfc0e7ea0374ea5ce77df1b7993feb8242
Nikhil Singh <nikhilsingh498@gmail.com>
2019-08-02 23:04:01 +0530

    Add preloader on index.html

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,20 @@
 //     document.getElementById("popup-description").innerHTML = "We will be soon here";
 // })
 
-document.getElementById("close-btn").addEventListener("click", (e) => {
-    document.getElementById("popup").style.display = "none";
-})
+
+document.getElementById("preloader").style.display = "block";
+
+window.onload = () => {
+    document.getElementById("preloader").style.display = "none";
+
+
+    setTimeout( () => {
+        document.getElementById("close-btn").addEventListener("click", (e) => {
+            document.getElementById("popup").style.display = "none";
+        })
+    }, 2000)
+
+
+
+}
+
