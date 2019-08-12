d6fd13f7c858aceabdac62816dd4955c03da2cab
Nikhil Singh <nikhilsingh498@gmail.com>
2019-08-13 04:15:59 +0530

    Add popup on reg click

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,3 @@
-// document.getElementById("blog").addEventListener("click", (e) => {
-//     document.getElementById("popup").style.display = "block";
-//     document.getElementById("popup").classList.add("animated", "fadeIn", "fast");
-//     document.getElementById("popup-img").src = "../asset/images/comingsoon.gif";
-//     document.getElementById("popup-head").innerHTML = "Coming Soon";
-//     document.getElementById("popup-description").innerHTML = "We will be soon here";
-// })
-
 
 document.getElementById("preloader").style.display = "block";
 
@@ -23,3 +15,6 @@ window.onload = () => {
 
 }
 
+document.getElementById("reg-popup").addEventListener("click", (e) => {
+    document.getElementById("popup").style.display = "block";
+})
